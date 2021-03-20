ee4a953772e2e897248098c79c5bad33a980e38c
saebom <boma91@gmail.com>
2021-03-20 15:05:57 +0900

    Add: brand page 완성

--- main/index.js
+++ main/index.js
@@ -1,5 +1,78 @@
 'use strict';
 
+// nav drop-down
+const menu = document.querySelectorAll('.sub_li a');
+const drop = document.querySelectorAll('.dropdown')
+const dropdown = document.querySelectorAll('.dropdown_li');
+
+menu.forEach(item => {
+    //console.log(item)
+    item.addEventListener('mouseover', shwoDropLists)
+    dropdown.forEach(item => {
+        item.addEventListener('mouseover', shwoDropLists)
+    })
+})
+
+menu.forEach(item => {
+    item.addEventListener('mouseout', hideDropLists)
+    dropdown.forEach(item => {
+        item.addEventListener('mouseout', hideDropLists)
+    })
+})
+
+function shwoDropLists(e) {
+
+    let hoverNav;
+
+    if (e.path[0].localName === 'a') {
+        hoverNav = e.target.nextElementSibling.children
+    }
+    else if (e.path[0].localName === 'li') {
+        hoverNav = e.target.parentElement.children
+    }
+
+
+    for (let i = 0; i < hoverNav.length; i++) {
+        hoverNav[i].classList.add('show');
+    }
+
+    //console.log(e.path[0].localName)
+
+
+
+
+    // 전체 dropdown되는 공식
+    // for (let i = 0; i < dropdown.length; i++) {
+    //     dropdown[i].classList.add('show');
+    // }
+
+
+    // console.log(e.target.nextElementSibling.children[0])
+
+}
+
+function hideDropLists(e) {
+
+    let hoverNav;
+
+    if (e.path[0].localName === 'a') {
+        hoverNav = e.target.nextElementSibling.children
+    }
+    else if (e.path[0].localName === 'li') {
+        hoverNav = e.target.parentElement.children
+    }
+
+
+    for (let i = 0; i < hoverNav.length; i++) {
+        hoverNav[i].classList.remove('show');
+    }
+}
+
+
+
+
+
+
 // main > image slide
 let slideIndex = 1;
 let slides = document.querySelectorAll('.slide_li');
